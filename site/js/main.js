965d888fb06e5da4120a820d3acfbde12a3a6e9d
hectorpina <hectyamp@gmail.com>
2015-12-10 22:16:25 -0430

    v2

--- site/js/main.js
+++ site/js/main.js
@@ -1,37 +1,25 @@
-$(function() {
-	$( '#ri-grid' ).gridrotator( {
-					w320 : {
-						rows : 3,
-						columns : 4
-					},
-					w240 : {
-						rows : 3,
-						columns : 3
-					}
-				} );
 
-			});
-
-$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    nav:true,
-			dots:false,
-			autoplay:true,
-			autoplayHoverPause:true,
-			center:true,
-			responsiveClass:true,
-			lazyLoad:true,
-			mouseDrag:true
-
-	})
-	// collagePlus-ify it!
-  $('.Collage').collagePlus({
-	'allowPartialLastRow' : false,
-	'direction': 'horizontal',
-	'targetHeight'    : 800
+  window._wpemojiSettings = {"baseUrl":"http:\/\/s.w.org\/images\/core\/emoji\/72x72\/","ext":".png","source":{"concatemoji":"http:\/\/demo.flothemes.com\/rebecca\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.3.1"}};
+  !function(a,b,c){function d(a){var c=b.createElement("canvas"),d=c.getContext&&c.getContext("2d");
+  return d&&d.fillText?(d.textBaseline="top",d.font="600 32px Arial","flag"===a?(d.fillText(String.fromCharCode(55356,56812,55356,56807),0,0),c.toDataURL().length>3e3):(d.fillText(String.fromCharCode(55357,56835),0,0),0!==d.getImageData(16,16,1,1).data[0])):!1}
+function e(a){
+  var c=b.createElement("script");c.src=a,c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)
 }
+var f,g;c.supports={simple:d("simple"),flag:d("flag")},c.DOMReady=!1,c.readyCallback=function(){
+  c.DOMReady=!0},c.supports.simple&&c.supports.flag||(g=function(){c.readyCallback()
+},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()
+})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
 
-	);
 
 
-$("#mygallery").justifiedGallery();
+
+var floScaleSliderWidth = "1800";
+var floScaleSliderHeight = "1200";
+var floScaleSliderWidthVideo = "1070";
+var floScaleSliderHeightVideo = "570";
+var slideshow_autoplay = "1";
+var main_slideshow_autoplay_dalay = "3000";
+var main_slideshow_show_bullets = "yes";
+var main_slideshow_show_arrow = "no";
+var ajaxurl = "http:\/\/demo.flothemes.com\/rebecca\/wp-admin\/admin-ajax.php";
+var flo_js_data = {"header_1_menu_spacing_nonce":"d74acf3ea4","site_width":"1130","header_logo_style":"header_style4","header_1_menu_spacing":"275.17px","header_4_menu_left_spacing":"100px","header_4_menu_right_spacing":"72px","header_4_menu_spacing_nonce":"975b94dae1","single_gallery_autoplay":"0","single_gallery_autoplay_dalay":"3000"};
