b72acac6732bcd843814941edb39d1c9a73cbc91
hectorpina <hectyamp@gmail.com>
2015-12-15 16:19:41 -0430

    work update!

--- site/js/main.js
+++ site/js/main.js
@@ -1,5 +1,5 @@
 
-  window._wpemojiSettings = {"baseUrl":"http:\/\/s.w.org\/images\/core\/emoji\/72x72\/","ext":".png","source":{"concatemoji":"http:\/\/demo.flothemes.com\/rebecca\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.3.1"}};
+  window._wpemojiSettings = {"baseUrl":"http:\/\/s.w.org\/images\/core\/emoji\/72x72\/","ext":".png","source":{"concatemoji":""}};
   !function(a,b,c){function d(a){var c=b.createElement("canvas"),d=c.getContext&&c.getContext("2d");
   return d&&d.fillText?(d.textBaseline="top",d.font="600 32px Arial","flag"===a?(d.fillText(String.fromCharCode(55356,56812,55356,56807),0,0),c.toDataURL().length>3e3):(d.fillText(String.fromCharCode(55357,56835),0,0),0!==d.getImageData(16,16,1,1).data[0])):!1}
 function e(a){
@@ -21,5 +21,5 @@ var slideshow_autoplay = "1";
 var main_slideshow_autoplay_dalay = "3000";
 var main_slideshow_show_bullets = "yes";
 var main_slideshow_show_arrow = "no";
-var ajaxurl = "http:\/\/demo.flothemes.com\/rebecca\/wp-admin\/admin-ajax.php";
+var ajaxurl = "";
 var flo_js_data = {"header_1_menu_spacing_nonce":"d74acf3ea4","site_width":"1130","header_logo_style":"header_style4","header_1_menu_spacing":"275.17px","header_4_menu_left_spacing":"100px","header_4_menu_right_spacing":"72px","header_4_menu_spacing_nonce":"975b94dae1","single_gallery_autoplay":"0","single_gallery_autoplay_dalay":"3000"};
